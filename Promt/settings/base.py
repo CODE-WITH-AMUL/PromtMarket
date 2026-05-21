ade9859084541524896eb4facb519c7fa6341a10
Amul Sharma <iamelectord@gmail.com>
2026-05-21 09:23:59 +0545

    Big data config

--- Promt/settings/base.py
+++ Promt/settings/base.py
@@ -102,7 +102,7 @@ DATABASES = {
     )
 }
 
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ---------------- PASSWORDS ----------------
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
